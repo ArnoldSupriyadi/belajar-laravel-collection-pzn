ebb2cc6a47d2790c34bf1f47806b746db8f06b99
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-10-06 10:03:49 +0700

    125. Partitioning

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -86,4 +86,121 @@ class CollectionTest extends TestCase
             "HR" => collect(["Imam"])
         ], $result->all());
     }
+
+    public function testZip()
+    {
+        $collection1 = collect([1,2,3]);
+        $collection2 = collect([4,5,6]);
+        $collection3 = $collection1->zip($collection2);
+
+        assertEquals([
+            collect([1,4]),
+            collect([2,5]),
+            collect([3,6]),
+        ], $collection3->all());
+    }
+
+    public function testConcat()
+    {
+        $collection1 = collect([1,2,3]);
+        $collection2 = collect([4,5,6]);
+        $collection3 = $collection1->concat($collection2);
+
+        assertEquals([1,2,3,4,5,6], $collection3->all());
+
+    }
+
+    public function testCombine()
+    {
+        $collection1 = ["name", "country"];
+        $collection2 = ["Eko", "Indonesia"];
+        $collection3 = collect($collection1)->combine($collection2);
+
+        $this->assertEqualsCanonicalizing([
+            "name" => "Eko",
+            "country" => "Indonesia"
+        ], $collection3->all());
+    }
+
+    public function testCollapse()
+    {
+        $collection = collect([
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+        ]);
+
+        $result = $collection->collapse();
+        assertEquals([1,2,3,4,5,6,7,8,9], $result->all());
+    }
+
+    public function testMap()
+    {
+        $collection = collect([
+            [
+                "name" => "Eko",
+                "hobbies" => ["Coding", "Gaming"]
+            ],
+            [
+                "name" => "Khannedy",
+                "hobbies" => ["Reading", "Writing"]
+            ]
+        ]);
+
+        $hobbies = $collection->flatMap(function ($item){
+            return $item["hobbies"];
+        });
+
+        assertEquals(["Coding","Gaming","Reading","Writing"], $hobbies->all());
+    }
+
+    public function testJoin()
+    {
+        $collection = collect(["Eko", "Khannedy", "Arnold"]);
+
+        assertEquals("Eko-Khannedy-Arnold", $collection->join("-"));
+        assertEquals("Eko-Khannedy_Arnold", $collection->join("-", "_"));
+    }
+
+    public function testFilter()
+    {
+        $collection = collect([
+            "Eko" => 100,
+            "Budi" => 80,
+            "Joko" => 90
+        ]);
+        $result = $collection->filter(function ($value, $key){
+            return $value >=90;
+        });
+
+        assertEquals([
+            "Eko" => 100,
+            "Joko" => 90
+        ], $result->all());
+    }
+
+    public function testFilterIndex()
+    {
+        $collection = collect([1,2,3,4,5,6,7,8,9,10]);
+        $result = $collection->filter(function ($value, $key){
+            return $value % 2 == 0;
+        });
+
+        $this->assertEqualsCanonicalizing([2,4,6,8,10], $result->all());
+    }
+
+    public function testPartition()
+    {
+        $collection = collect([
+            "Eko" => 100,
+            "Budi" => 80,
+            "Joko" => 90
+        ]);
+        [$result1, $result2] = $collection->partition(function ($value, $key){
+            return $value >= 90;
+        });
+
+        assertEquals(["Eko" => 100, "Joko" => 90], $result1->all());
+        assertEquals(["Budi" => "80"], $result2->all());
+    }
 }
